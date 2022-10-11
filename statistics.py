24c444b2c426ae218a942348f2faca14b52cae68
Souravkumar2434 <115540932+Souravkumar2434@users.noreply.github.com>
2022-10-11 15:28:45 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,7 +1,11 @@
 
 def calculateStats(numbers):
-  num_sum = sum(numbers)
-  avg = num_sum / len(numbers)
-  max_num = max(numbers)
-  min_num = min(numbers)
+  avg, max, min = 0,0,0
+  if len(numbers) != 0:
+    num_sum = sum(numbers)
+    avg = num_sum / len(numbers)
+    max_num = max(numbers)
+    min_num = min(numbers)
+  else:
+    print("Input is empty")
   return avg, max_num, min_num
